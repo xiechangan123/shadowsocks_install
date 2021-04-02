9b09388dcae0a1f49cae290354a67d979eb3a43b
xiechangan123 <xiechangan123@139.com>
2021-04-02 13:37:53 +0800

    Update shadowsocks-libev-debian.sh

--- shadowsocks-libev-debian.sh
+++ shadowsocks-libev-debian.sh
@@ -15,8 +15,8 @@ cur_dir=`pwd`
 libsodium_file="libsodium-1.0.18"
 libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz"
 
-mbedtls_file="mbedtls-mbedtls-2.25.0"
-mbedtls_url="https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.25.0.tar.gz"
+mbedtls_file="mbedtls-mbedtls-2.26.0"
+mbedtls_url="https://github.com/ARMmbed/mbedtls/archive/mbedtls-2.26.0.tar.gz"
 
 # Stream Ciphers
 ciphers=(
