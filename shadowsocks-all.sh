a63903aeb9bf033034defba8d2ed52a63f55ab93
xiechangan123 <xiechangan123@139.com>
2023-01-13 16:24:49 +0800

    Update shadowsocks-all.sh

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -40,8 +40,8 @@ software=(Shadowsocks-Python ShadowsocksR Shadowsocks-Go Shadowsocks-libev)
 libsodium_file='libsodium-1.0.18'
 libsodium_url='https://github.com/jedisct1/libsodium/releases/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz'
 
-mbedtls_file='mbedtls-2.28.1'
-mbedtls_url='https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v2.28.1.tar.gz'
+mbedtls_file='mbedtls-2.28.2'
+mbedtls_url='https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v2.28.2.tar.gz'
 
 shadowsocks_python_file='shadowsocks-master'
 shadowsocks_python_url='https://github.com/shadowsocks/shadowsocks/archive/master.zip'
