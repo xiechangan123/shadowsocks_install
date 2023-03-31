96e6934f3bdb4fe36ab226bba78927f2d1db68df
xiechangan123 <xiechangan123@139.com>
2023-03-31 17:15:48 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -15,8 +15,8 @@ cur_dir=$(pwd)
 libsodium_file='libsodium-1.0.18'
 libsodium_url='https://github.com/jedisct1/libsodium/releases/download/1.0.18-RELEASE/libsodium-1.0.18.tar.gz'
 
-mbedtls_file='mbedtls-2.28.2'
-mbedtls_url='https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v2.28.2.tar.gz'
+mbedtls_file='mbedtls-2.28.3'
+mbedtls_url='https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v2.28.3.tar.gz'
 
 # Stream Ciphers
 ciphers=(
