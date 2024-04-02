0d624e917e5262f97f7fae67d1b10bcb112298ef
xiechangan123 <xiechangan123@139.com>
2024-04-02 19:18:56 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -12,7 +12,7 @@ export PATH
 # Current folder
 cur_dir=$(pwd)
 
-libsodium_file='libsodium-1.0.19'
+libsodium_file='libsodium-stable'
 libsodium_url='https://github.com/jedisct1/libsodium/releases/download/1.0.19-RELEASE/libsodium-1.0.19.tar.gz'
 
 mbedtls_file='mbedtls-2.28.8'
